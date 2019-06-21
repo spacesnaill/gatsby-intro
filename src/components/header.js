6ceabbc55a157403737c9b93b190b6e0bf4902ed
Patrick Geiger <patrickjgeiger@gmail.com>
2019-06-21 10:05:48 -0400

    filled out header component with links to different pages and styled the navbar

--- src/components/header.js
+++ src/components/header.js
@@ -2,9 +2,27 @@ import React from 'react';
 import { css } from '@emotion/core';
 import NavLink from './NavLink';
 
-const Header = () => {
-  <header>
-    {/** site name */}
+const Header = () => (
+  <header
+    css={css`
+      background: #eee;
+      border-bottom: 1px solid #ddd;
+      display: flex;
+      justify-content: space-between;
+      padding: 0.5rem; calc((100vw - 550px) / 2);
+    `}
+  >
+    <NavLink to="/">FEM Workshop</NavLink>
+    <nav
+      css={css`
+        margin-top: 0;
+      `}
+    >
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/about">About</NavLink>
+    </nav>
     {/** navigation */}
-  </header>;
-};
+  </header>
+);
+
+export default Header;
