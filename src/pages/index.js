701b63e27f27ccd5bb25685b291f4e3b2b260b0b
Patrick Geiger <patrickjgeiger@gmail.com>
2019-06-21 09:50:54 -0400

    added global styles to both about and index via layout component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import Layout from '../components/layout';
 
 export default () => (
-  <>
+  <Layout>
     <h1>Home</h1>
     <p>Hello Minnesota!</p>
     <Link to="/about/">Learn About Me &rarr;</Link>
-  </>
+  </Layout>
 );
