f4434b5058460eae7593ef72dd1abc3307d8885e
Patrick Geiger <patrickjgeiger@gmail.com>
2019-06-21 09:51:08 -0400

    added header component

---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { css } from '@emotion/core';
+import NavLink from './NavLink';
+
+const Header = () => {
+  <header>
+    {/** site name */}
+    {/** navigation */}
+  </header>;
+};
