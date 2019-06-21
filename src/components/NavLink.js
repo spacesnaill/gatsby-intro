1e08004d86a17c483082894daa0165e805f471b9
Patrick Geiger <patrickjgeiger@gmail.com>
2019-06-21 09:51:16 -0400

    added NavLink component

---
+++ src/components/NavLink.js
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+import { Link } from 'gatsby';
+
+const NavLink = styled(Link)`
+  color: #222;
+  font-size: 1rem;
+  font-weight: normal;
+  line-height: 1;
+  margin: 0 0.5rem 0 0;
+  padding: 0.25rem;
+  text-decoration: none;
+`;
+
+export default NavLink;
