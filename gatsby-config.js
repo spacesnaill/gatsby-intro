5923fa01c49b710d86328b30c4001649b0e42c45
Patrick Geiger <patrickjgeiger@gmail.com>
2019-06-22 15:43:06 -0400

    added mdx and set it up for use in the gatsby config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,16 @@ module.exports = {
     title: 'Gatsby Blog',
     description: 'A blog a build in Gatsby. Still under construction!',
   },
-  plugins: ['gatsby-plugin-emotion', 'gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-mdx',
+      options: {
+        defaultLayouts: {
+          default: require.resolve('./src/components/layout.js'),
+        },
+      },
+    },
+  ],
 };
