fea2ea545b43b2da0ec50b5155560d56c83ebf08
Patrick Geiger <patrickjgeiger@gmail.com>
2019-06-23 14:34:16 -0400

    added Hero component to the home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,20 +3,24 @@ import { Link } from 'gatsby';
 import Layout from '../components/layout';
 import usePosts from '../hooks/use-posts';
 import PostPreview from '../components/post-preview';
+import Hero from '../components/hero';
 
 export default () => {
   const posts = usePosts();
 
   return (
-    <Layout>
-      <h1>Home</h1>
-      <p>Hello Minnesota!</p>
-      <Link to="/about/">Learn About Me &rarr;</Link>
+    <>
+      <Hero></Hero>
+      <Layout>
+        <h1>Home</h1>
+        <p>Hello Minnesota!</p>
+        <Link to="/about/">Learn About Me &rarr;</Link>
 
-      <h2>Read my blog</h2>
-      {posts.map(post => (
-        <PostPreview key={post.slug} post={post}></PostPreview>
-      ))}
-    </Layout>
+        <h2>Read my blog</h2>
+        {posts.map(post => (
+          <PostPreview key={post.slug} post={post}></PostPreview>
+        ))}
+      </Layout>
+    </>
   );
 };
