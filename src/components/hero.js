84d5bf271d4408a446e50cea557f67f1400fe1dd
Patrick Geiger <patrickjgeiger@gmail.com>
2019-06-23 15:01:24 -0400

    using Sharp and GraphQL to provide the images instead of pulling them directly
    also, what is sent is a webp instead of a jpg

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import { Link, graphql, useStaticQuery } from 'gatsby';
+import BackgroundImage from 'gatsby-background-image';
 
-const ImageBackground = styled('div')`
-  background-image: url('/images/gregory-hayes-mn.jpg');
+const ImageBackground = styled(BackgroundImage)`
   background-position: top 20% center;
   background-size: cover;
   height: 25vh;
@@ -38,8 +38,20 @@ const TextBox = styled('div')`
 `;
 
 const Hero = () => {
+  const { image } = useStaticQuery(graphql`
+    query {
+      image: file(relativePath: { eq: "gregory-hayes-mn.jpg" }) {
+        sharp: childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+    }
+  `);
+
   return (
-    <ImageBackground>
+    <ImageBackground Tag="section" fluid={image.sharp.fluid} fadeIn>
       <TextBox>
         <h1>Frontend Masters + Gatsby &hearts;</h1>
         <p>
